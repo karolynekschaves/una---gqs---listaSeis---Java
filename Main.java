28c978fba60d9b80d92ca0a18763b7cc7d77956b
Karolyne Silva <karolynekschaves@gmail.com>
2026-09-04 20:09:09 -0300

    comitando meu arquivo java

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Mensagem de boas-vindas
+        System.out.println("Olá, Mundo! Este é um arquivo Java simples.");
+        
+        // Exemplo simples de calculo
+        int a = 10;
+        int b = 5;
+        int soma = a + b;
+        
+        System.out.println("A soma de " + a + " e " + b + " é igual a: " + soma);
+    }
+}
